b6a14e941a8a8fee1f259da4d38d0b512fcd8363
Ganesh Upare <ganeshupare3108@gmail.com>
2026-08-20 13:50:16 +0530

    updated

--- Diff_array_problem.js
+++ Diff_array_problem.js
@@ -6,21 +6,30 @@ let i=0
 let j=arr.length-1
 
 let diff
+let small_diff=Infinity
 while(i<j){
 
      if(arr[i]+arr[j]===target){
         console.log(arr[i],arr[j],"diffrence 0")
         break
     }
-    if(arr[i]+arr[j]>target){
+    if((arr[i]+arr[j])>target){
         diff=arr[i]+arr[j]-target
+        if(diff<small_diff)
+        {
+            small_diff=diff
+        }
         j--
     }
     else{
-        diff=target-arr[i]+arr[j]
+        diff=target-(arr[i]+arr[j])
+        if(diff<small_diff)
+        {
+            small_diff=diff
+        }
         i++
     }
    
 }
 
-console.log(diff)
+console.log(small_diff)
